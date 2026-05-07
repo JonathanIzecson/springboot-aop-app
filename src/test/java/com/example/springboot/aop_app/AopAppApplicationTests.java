c101c2856a4ae416db52348c947e62d957003673
Jonathan Escobedo <sistemas@sicammexico.com.mx>
2026-05-07 14:05:20 -0600

    Init repo

---
+++ src/test/java/com/example/springboot/aop_app/AopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.aop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
